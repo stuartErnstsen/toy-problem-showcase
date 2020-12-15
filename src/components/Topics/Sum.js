d538c66c2cecc88345ccf4692ca3d6d1d472b13c
stuartErnstsen <stuarternstsen@gmail.com>
2020-12-14 22:32:10 -0700

    Completed remaining functionality in Sum Component

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 class Sum extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            calculated: 0,
+            userInput: ''
+        }
+    }
+
+    handleChange(input) {
+        this.setState({ userInput: input })
+    }
+
+    sumNums(numStr) {
+        const numArr = numStr.split(',');
+        this.setState({ calculated: numArr.reduce((acc, e) => { return acc + parseInt(e, 10) }, 0) })
+    }
+
     render() {
         return (
-            <p>Sum component.</p>
+            <div className="puzzleBox sumPB">
+                <h4> Sum Component </h4>
+                <input className="inputLine" onChange={e => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick={() => this.sumNums(this.state.userInput)}></button>
+                <span className="resultsBox">{this.state.calculated}</span>
+            </div>
         );
     };
 };
